fdc87460945e5705ce305a319b864830869b9e07
CrashOverr1de <dongmin85@gmail.com>
2020-04-20 18:21:39 +0900

    apply separate routes

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 var express = require('express')
 var app = express()
+var indexRouter = require('./routes/index')
+
 var hostname = 'localhost'
 var port = 3000
 
 app.set('view engine', 'ejs')
-
-app.get('/', function(req, res) {
-  res.render('index/index', { message: 'Hello, World!' })
-})
+app.use('/', indexRouter)
 
 app.listen(port, hostname, function() {
   console.log(`Server running at http://${hostname}:${port}`)
