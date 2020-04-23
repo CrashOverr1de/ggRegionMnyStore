09562db72d6cca9fd298f29f5979588034463eab
CrashOverr1de <dongmin85@gmail.com>
2020-04-23 14:10:09 +0900

    modify getData router, search result view component

--- routes/getData/getData.js
+++ routes/getData/getData.js
@@ -3,11 +3,26 @@ var router = express.Router()
 
 router.get('/RegionMnyFacltStusList', function(req, res) {
   var _data = req.app.get('data')
+
+  var result = []
+  result.push(_data[0])
+  for(var i = 1; i < _data.length; i++) {
+    if(_data[i].REFINE_LOTNO_ADDR !== null && _data[i].REFINE_LOTNO_ADDR.indexOf('갈매') > -1) {
+      result.push(_data[i])
+    }
+  }
+  
   // res.json(data)
+  res.render('components/index/DataListRender', {
+    head: result[0],
+    data: result
+  })
+  /*
   res.render('components/index/DataListRender', {
     head: _data[0],
     data: _data
   })
+  */
   /*
   res.render('components/index/DataListRender', {
     head: data.RegionMnyFacltStus[0].head,
