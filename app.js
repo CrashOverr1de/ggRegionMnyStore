0e06695e9a8f136f43429aa671631d468155cec8
CrashOverr1de <dongmin85@gmail.com>
2020-04-23 12:44:29 +0900

    modify data initialization process

--- app.js
+++ app.js
@@ -10,28 +10,16 @@ app.set('view engine', 'ejs')
 app.use('/', indexRouter)
 app.use('/getData', getRouter)
 
-// 어차피 데이터가 거의 정적이고 동네 것만 볼 것이기 때문에
-// 메인 데이터는 WAS 시작할 때 한 번 가져와서 메모리에 넣어둔다.
 init.data().then(function(data) {
-  // 이렇게 넣어두고 분리된 라우터들에서 request 객체를 통해 언제든지 가져다 쓸 수 있다.
-  // req.app.get('data') 이렇게~
   app.set('data', data)
+}).then(function() {
+  app.set('data', init.rebuild(app.get('data')))
+  if(app.get('data')) console.log('done..!')
+  else console.log('failure data setting')
 }).catch(function(err) {
   console.log(err)
 })
 
 app.listen(conf.env.port, conf.env.hostname, function() {
   console.log(`Server running at http://${conf.env.hostname}:${conf.env.port}/`)
-  /* ./config/config.json
-  {
-    "env": {
-      "hostname": "localhost",
-      "port": 3000
-    },
-    "api": {
-      "url": "https://api.example.com/blahblah",
-      "key": "Your API Key"
-    }
-  }
-  */
 })
--- routes/getData/getData.js
+++ routes/getData/getData.js
@@ -2,12 +2,18 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/RegionMnyFacltStusList', function(req, res) {
-  var data = req.app.get('data')
+  var _data = req.app.get('data')
   // res.json(data)
+  res.render('components/index/DataListRender', {
+    head: _data[0],
+    data: _data
+  })
+  /*
   res.render('components/index/DataListRender', {
     head: data.RegionMnyFacltStus[0].head,
     data: data.RegionMnyFacltStus[1].row
   })
+  */
 })
 
 module.exports = router
